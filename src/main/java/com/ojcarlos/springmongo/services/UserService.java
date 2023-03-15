4a08d10fe195f8e0bd5228de993352668e8f8e40
ojcarlos <carlos.ojunior00@gmail.com>
2023-03-15 16:46:16 -0400

    delete user

--- src/main/java/com/ojcarlos/springmongo/services/UserService.java
+++ src/main/java/com/ojcarlos/springmongo/services/UserService.java
@@ -29,6 +29,12 @@ public class UserService {
         return repo.insert(user);
     }
 
+    public void delete(String id){
+
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDto (UserDto userDto){
         return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
