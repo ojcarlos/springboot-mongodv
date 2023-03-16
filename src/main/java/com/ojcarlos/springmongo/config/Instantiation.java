79a2b73733668fc80b3f8d6237ae824f0d587a70
ojcarlos <carlos.ojunior00@gmail.com>
2023-03-16 16:58:27 -0400

    post dto author

--- src/main/java/com/ojcarlos/springmongo/config/Instantiation.java
+++ src/main/java/com/ojcarlos/springmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.ojcarlos.springmongo.config;
 
 import com.ojcarlos.springmongo.domain.Post;
 import com.ojcarlos.springmongo.domain.User;
+import com.ojcarlos.springmongo.dto.AuthorDto;
 import com.ojcarlos.springmongo.repository.PostRepository;
 import com.ojcarlos.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,18 +27,21 @@ public class Instantiation implements CommandLineRunner {
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post p1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post p2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia!", "Acordei feliz", maria);
+        Post p1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
+
+        Post p2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia!", "Acordei feliz", new AuthorDto(maria));
 
         postRepository.saveAll(Arrays.asList(p1,p2));
 
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
 
 
     }
--- src/main/java/com/ojcarlos/springmongo/domain/Post.java
+++ src/main/java/com/ojcarlos/springmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.ojcarlos.springmongo.domain;
 
+import com.ojcarlos.springmongo.dto.AuthorDto;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,13 +19,13 @@ public class Post implements Serializable {
     private String title;
     private String body;
 
-    private User author;
+    private AuthorDto author;
 
     public Post() {
     }
 
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDto author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -64,11 +65,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDto getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDto author) {
         this.author = author;
     }
 
